121a01a1b72532dad6536faa1d48bc845267c0b7
bibekbibekbibek <raibibek122@gmail.com>
2025-02-21 12:57:39 +0545

    elearning

--- elearn/settings.py
+++ elearn/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env(
-)
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
